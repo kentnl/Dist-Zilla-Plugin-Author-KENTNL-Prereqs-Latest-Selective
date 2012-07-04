05dadd2a5eefc0194a6be0ec00d3ceabed5ccca3
Kent Fredric <kentfredric@gmail.com>
2012-07-04 23:36:05 +1200

    Build results of 46f275c (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/Prereqs/Latest/Selective.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/Prereqs/Latest/Selective.pm
@@ -73,6 +73,7 @@ sub register_prereqs {
 		my ( $self, $args ) = @_;
 		my $package = $args->{package};
 		if ( exists $self->wanted_latest->{$package} ) {
+			print "\e[31m Upgraded $package \e[0m\n";
 			$self->register_prereqs(
 				{ phase => $args->{phase}, type => $args->{type}},
 				$package , $self->current_version_of( $package ),
