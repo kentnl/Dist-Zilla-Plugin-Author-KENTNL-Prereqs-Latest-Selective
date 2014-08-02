056309eb9fd54baa37fcd48b98817a54554b24f9
Kent Fredric <kentfredric@gmail.com>
2014-08-02 16:52:19 +1200

    switch to x.yyyyyy

--- lib/Dist/Zilla/Plugin/Author/KENTNL/Prereqs/Latest/Selective.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/Prereqs/Latest/Selective.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Author::KENTNL::Prereqs::Latest::Selective;
 
-our $VERSION = '0.1.3';
+our $VERSION = '1.000000';
 
 # ABSTRACT: [DEPRECATED] Selectively upgrade a few modules to depend on the version used.
 
